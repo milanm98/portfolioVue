9fee7c7ef66f6434192126f1a495819a6a5720dd
milan-marjanov <milos.milovanovic@pbssolutions.net>
2023-06-10 22:42:06 +0200

    delete unused code

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
 <template>
-  <header></header>
+  <header>
+  </header>
+
+
+  <RouterView/>
 </template>
--- portfolio/src/views/HomeView.vue
+++ portfolio/src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="gold">
+      Welcome to my portfolio website. I'm a web developer who is proficient in
+      learning and working with Node.js, React, and AWS. Let's collaborate and create impactful web
+      solutions together!
+    </h1>
   </main>
 </template>
