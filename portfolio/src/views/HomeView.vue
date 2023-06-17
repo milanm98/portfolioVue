e8515bd7a64b9c5ddc434bee97c6f914a816f683
milan-marjanov <milos.milovanovic@pbssolutions.net>
2023-06-17 14:11:04 +0200

    add text styles

--- portfolio/src/views/HomeView.vue
+++ portfolio/src/views/HomeView.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>
-    <h1 class="gold">
-      Welcome to my portfolio website. I'm a web developer who is proficient in
-      learning and working with Node.js, React, and AWS. Let's collaborate and create impactful web
-      solutions together!
+  <main class="flex flex-col lg:flex-row justify-center items-center">
+    <h1
+      class="p-5 lg:p-12 gold text-2xl lg:text-4xl whitespace-normal leading-10 lg:leading-relaxed m-1 lg:m-12 shadow-2xl shadow-blue-500/50"
+    >
+      Welcome to <span class="turquoise underline">my portfolio</span> website. I'm a web developer
+      who is proficient in learning and working with
+      <span class="turquoise underline">Node.js</span>,
+      <span class="turquoise underline">React</span>, and
+      <span class="turquoise underline">AWS</span>
+      . Please feel free to <span class="turquoise underline">contact</span> me so we can explore
+      opportunities to <span class="turquoise underline">work</span> together.
     </h1>
   </main>
 </template>
